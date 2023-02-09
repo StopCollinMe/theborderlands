5389b8bab24d92d3b6e8ad4ae3c1a4e8f92ef55c
Collin Uemura <collinuemura@gmail.com>
2023-02-08 20:22:36 -0800

    Took return statement out of the for loop to show more than two cards

--- index.js
+++ index.js
@@ -15,6 +15,11 @@ const collapsedGridAreaTemplate = '"a a" "a a"';
 
 const cardCollectionCellClass = ".card-pos-a";
 
+const numCards = cardObjectDefinitions.length;
+
+let cardPositions = [];
+
+
 loadGame();
 
 function loadGame(){
@@ -38,7 +43,8 @@ function initializeNewGame(){
 function startRound(){
   initializeNewRound();
   collectCards();
-  flipCards(true);
+  //flipCards(true);
+  shuffleCards();
 }
 
 function initializeNewRound(){
@@ -83,6 +89,77 @@ function flipCards(flipToBack){
   })
 }
 
+function shuffleCards(){
+  const id = setInterval(shuffle, 12);
+  let shuffleCount = 0;
+
+  function shuffle(){
+
+    randomizeCardPositions();
+
+    if(shuffleCount == 500){
+      clearInterval(id);
+      dealCards();
+    }
+    else{
+      shuffleCount++;
+    }
+  }
+}
+
+function randomizeCardPositions(){
+  const random1 = Math.floor(Math.random() * numCards) + 1;
+  const random2 = Math.floor(Math.random() * numCards) + 1;
+
+  const temp = cardPositions[random1 - 1];
+  cardPositions[random1 - 1] = cardPositions[random2 - 1];
+  cardPositions[random2 - 1] = temp;
+
+}
+
+//Check this in the video
+function dealCards(){
+  addCardsToAppropriateCell();
+  const areasTemplate = returnGridAreasMappedToCardPos();
+
+  transformGridArea(areasTemplate);
+}
+
+function returnGridAreasMappedToCardPos(){
+  let firstPart = "";
+  let secondPart = "";
+  let areas = "";
+
+  cards.forEach((card, index)=>{
+    if(cardPositions[index] == 1){
+      areas = areas + "a ";
+    }
+    else if(cardPositions[index] == 2){
+      areas = areas + "b ";
+    }
+    else if(cardPositions[index] == 3){
+      areas = areas + "c ";
+    }
+    else if(cardPositions[index] == 4){
+      areas = areas + "d ";
+    }
+    if (index == 1){
+      firstPart = areas.substring(0, areas.length - 1);
+      areas = "";
+    }
+    else if (index == 3){
+      secondPart = areas.substring(0,areas.length - 1);
+    }
+    
+  })
+  return `"${firstPart}" "${secondPart}"`;
+}
+
+function addCardsToAppropriateCell(){
+  cards.forEach((card)=>{
+    addCardToGridCell(card);
+  })
+}
 
 
 function createCards(){
@@ -149,7 +226,14 @@ function createCard(cardItem){
   //add card element as child element to appropriate grid cell 
   addCardToGridCell(cardElem);
 
+  initializeCardPositions(cardElem);
+
 }
+
+function initializeCardPositions(card){
+  cardPositions.push(card.id);
+}
+
 
 function createElement(elemType){
   return document.createElement(elemType);
